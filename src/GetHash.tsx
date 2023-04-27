91e0d4e2ef592fce6ac9f63fe2701c1e51c65a52
Daniel Morales-Garcia <dmorales33@unm.edu>
2023-04-27 11:01:43 -0600

    Styled App.tsx/GetHash.tsx and added logo.

--- src/GetHash.tsx
+++ src/GetHash.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
+import logo from './logo.png'; // Import the logo image
 const { ipcRenderer } = window.require('electron');
 
-function NameForm() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [hash, setHash] = useState('');
+const Login: React.FC = () => {
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const [hash, setHash] = React.useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const data = { firstName, lastName };
-    setHash(`${firstName}${lastName}`)
+    setHash(`${firstName}${lastName}`);
     // ipcRenderer.invoke('hash-data', data).then((result) => {
     //   setHash(result);
     //   console.log(`Hash value: ${result}`);
@@ -19,31 +19,50 @@ function NameForm() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <form onSubmit={handleSubmit} className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Name Form</h1>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" for="first-name">
-            First Name:
-          </label>
-          <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="first-name" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" for="last-name">
-            Last Name:
-          </label>
-          <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="last-name" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        </div>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
-      </form>
-      {hash && (
-        <div className="mt-8">
-          <p className="text-gray-700 font-bold">Hash value:</p>
-          <p className="text-gray-700">{hash}</p>
-        </div>
-      )}
+    <div className="bg-gradient-to-tr from-green-500 to-blue-300 min-h-screen flex items-center justify-center">
+      <img src={logo} alt="Logo" className="absolute bottom-4 right-4 h-16 w-auto"/>
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h2 className="text-3xl font-semibold text-center mb-8">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-6">
+            <label className="block text-gray-600 text-sm mb-2" htmlFor="first-name">
+              First Name:
+            </label>
+            <input
+              className="w-full p-2 text-lg border-b-4 border-gray-300 focus:border-green-500 focus:outline-none focus:ring-0 transition duration-500"
+              id="first-name"
+              type="text"
+              placeholder="Enter your first name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-600 text-sm mb-2" htmlFor="last-name">
+              Last Name:
+            </label>
+            <input
+              className="w-full p-2 text-lg border-b-4 border-gray-300 focus:border-green-500 focus:outline-none focus:ring-0 transition duration-500"
+              id="last-name"
+              type="text"
+              placeholder="Enter your last name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="w-full text-lg bg-gradient-to-r from-green-500 to-blue-300 text-white font-semibold py-3 px-4 rounded-full focus:outline-none hover:from-green-600 hover:to-blue-400 transition duration-300">
+            Submit
+          </button>
+        </form>
+        {hash && (
+          <div className="mt-6 text-center">
+            <p className="text-gray-700 font-bold text-xl">Hash value:</p>
+            <p className="text-gray-700 text-lg">{hash}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default NameForm;
+export default Login;
