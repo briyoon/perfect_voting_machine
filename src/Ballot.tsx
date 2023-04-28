3240e236e4d0aa935ab7fdb738e3308ca5106e21
Daniel Morales-Garcia <dmorales33@unm.edu>
2023-04-28 12:28:28 -0600

    Styled Ballot

--- src/Ballot.tsx
+++ src/Ballot.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Contest from './components/Contest';
 import Proposition from './components/Proposition';
@@ -11,14 +11,6 @@ export default function BallotComp() {
   const [currentItemIndex, setCurrentItemIndex] = useState(0);
   const [ballotData, setBallotData] = useState<Ballot | null>(null);
   const [choices, setChoices] = useState<any[]>([]);
-
-  // const idk = (data, index) => {
-  //   if choices[index] === undefined {
-  //     choices[index] = data;
-  //   }
-  //   else
-  // }
-
 
   useEffect(() => {
     const fetchBallot = async () => {
@@ -73,24 +65,26 @@ export default function BallotComp() {
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center">
-      <div className="">
-        <p className="font-bold">{ballotData.header.title}</p>
-        <p className="">{`Current section: ${currentSection.sectionName}`}</p>
-        <p className="">{ballotData.header.instructions}</p>
-      </div>
-      <div className="flex-grow pt-48 w-full h-full flex flex-col items-center">
-        {renderCurrentItem()}
-      </div>
-      <div className="flex flex-row justify-end items-center ">
-        <button className="mx-4" onClick={handlePrev} disabled={currentSectionIndex === 0 && currentItemIndex === 0}>
-          Previous
-        </button>
-        <button className="mx-4" onClick={handleNext} disabled={currentSectionIndex === ballotData.sections.length - 1 && currentItemIndex === currentSection.items.length - 1}>
-          Next
-        </button>
+    <div className="bg-gradient-to-tr from-green-500 to-blue-300 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-2xl">
+        <div className="mb-4">
+          <p className="font-bold text-black text-center text-2xl">{ballotData.header.title}</p>
+          <p className="text-black text-center text-lg">{`Current section: ${currentSection.sectionName}`}</p>
+          <p className="text-black text-center text-lg">{ballotData.header.instructions}</p>
+        </div>
+        <hr className="border-t-2 border-gray-200 mb-4 mx-auto w-2/3" />
+        <div className="flex-grow w-full h-full flex flex-col items-center">
+          {renderCurrentItem()}
+        </div>
+        <div className="flex flex-row justify-center items-center mt-4">
+          <button className="bg-white border border-gray-300 rounded-full shadow-md w-40 h-[12%] m-4 text-2xl flex justify-center items-center transition duration-300 transform hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mx-4" onClick={handlePrev} disabled={currentSectionIndex === 0 && currentItemIndex === 0}>
+            Previous
+          </button>
+          <button className="bg-white border border-gray-300 rounded-full shadow-md w-40 h-[12%] m-4 text-2xl flex justify-center items-center transition duration-300 transform hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mx-4" onClick={handleNext} disabled={currentSectionIndex === ballotData.sections.length - 1 && currentItemIndex === currentSection.items.length - 1}>
+            Next
+          </button>
+        </div>
       </div>
     </div>
-
   );
 }
