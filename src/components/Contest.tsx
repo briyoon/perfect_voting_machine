3e9b68625d0c1e69ff399eb8dc5c8d3fe1813f1d
Austyn Mitchev <amitchev@unm.edu>
2023-04-29 17:29:35 -0600

    Added logic to store individual ballots

--- src/components/Contest.tsx
+++ src/components/Contest.tsx
@@ -1,17 +1,31 @@
 import type { FunctionComponent } from "react";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { BallotContext } from "@/Ballot";
 
 interface ContestProps {
     contest: Contest
 }
 
 const Contest: FunctionComponent<ContestProps> = ({ contest }) => {
+
+    const { currentChoice, setCurrentChoice } = useContext(BallotContext);
+    
+    const handleCurrChoice = (choice: any) => {
+        console.log(choice);
+        setCurrentChoice(choice);
+    }
+
+    const handleWriteIn = (event: any) => {
+        setCurrentChoice(event.target.value)
+    }
+
+
     return (
         <>
             <h2 className="text-2xl font-bold mb-4">{contest.contestName}</h2>
             <ul>
             {contest.contestChoices.map((choice, index) => (
-                <li key={index} className="mb-2">
+                <li onClick = {() => handleCurrChoice(choice.candidate.name)} key={index} className="mb-2">
                 <label className="inline-flex items-center">
                     <input type="radio" name="contest" value={index} className="form-radio h-4 w-4 text-indigo-600" />
                     <span className="ml-2">{choice.candidate.name} ({choice.candidate.party})</span>
@@ -24,7 +38,7 @@ const Contest: FunctionComponent<ContestProps> = ({ contest }) => {
                     <input type="radio" name="contest" value="write-in" className="form-radio h-4 w-4 text-indigo-600" />
                     <span className="ml-2">Write-in</span>
                 </label>
-                <input type="text" placeholder="Candidate name" className="ml-4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+                <input onChange = {handleWriteIn} type="text" placeholder="Candidate name" className="ml-4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
                 </li>
             )}
             </ul>
--- src/components/RankedChoice.tsx
+++ src/components/RankedChoice.tsx
@@ -1,20 +1,30 @@
 import type { FunctionComponent } from "react";
-import { useState } from "react";
-
+import { useContext, useState } from "react";
+import { BallotContext } from "@/Ballot";
 
 interface RankedChoiceProps {
     rankedChoice: RankedChoice
 }
 
 const RankedChoice: FunctionComponent<RankedChoiceProps> = ({ rankedChoice }) => {
+
+    const { currentChoice, setCurrentChoice } = useContext(BallotContext);
+    
+    const handleCurrChoice = (choice: any) => {
+        console.log(choice);
+        setCurrentChoice(choice);
+    }
+
+
     return(
         <>
             <h2 className="text-2xl font-bold mb-4">{rankedChoice.rankedChoiceName}</h2>
             <form>
             <ul>
                 {rankedChoice.rankedChoices.map((choice, index) => (
-                    <li key={index} className="mb-2">
-                    <span>{choice.candidate.name} - {choice.candidate.party}</span>
+                    <li onClick = {() => handleCurrChoice(choice.candidate.name)} key={index} className="mb-2">
+                        <input type="radio" name={`rankedChoice_${index}`} value={index} className="form-radio h-4 w-4 text-indigo-600" />
+                        <span> {choice.candidate.name} - {choice.candidate.party}</span>
                     </li>
                  ))}
             </ul>
